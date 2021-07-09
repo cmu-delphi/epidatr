c50a5b333630ff8c350afc0795aa346b20c26366
Samuel Gratzl <sam@sgratzl.com>
2021-07-09 10:40:57 +0200

    feat: add more examples

--- R/utils.R
+++ R/utils.R
@@ -1,17 +1,19 @@
 
 # Helper function to cast values and/or ranges to strings
 format_item <- function(value) {
-    if(is.list(value) && 'from' %in% names(value) && 'to' %in% names(value)) {
-      paste0(toString(value$from), '-', toString(value$to))
-    } else {
-      toString(value)
-    }
+  if(inherits(value, "EpiRange")) {
+    paste0(toString(value$from), '-', toString(value$to))
+  } else if(is.list(value) && 'from' %in% names(value) && 'to' %in% names(value)) {
+    paste0(toString(value$from), '-', toString(value$to))
+  } else {
+    toString(value)
   }
+}
 
 # Helper function to build a list of values and/or ranges
 format_list <- function(values) {
-    if(!is.list(values) || ('from' %in% names(values) && 'to' %in% names(values))) {
-      values <- list(values)
-    }
-    paste(sapply(values, format_item), collapse=',')
+  if(!is.list(values) || ('from' %in% names(values) && 'to' %in% names(values))) {
+    values <- list(values)
+  }
+  paste(sapply(values, format_item), collapse=',')
 }
