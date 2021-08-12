3fac63a0c4ff3d7a365b4d4d1ea24f54e6e0d714
Samuel Gratzl <sam@sgratzl.com>
2021-08-12 08:27:55 -0400

    ci: fix ci and add make file

--- R/constants.R
+++ R/constants.R
@@ -2,4 +2,4 @@
 
 version <- "1.0.0"
 http_headers <- httr::add_headers("User-Agent" = paste0("delphi_epidata", version))
-base_rul <- "https://delphi.cmu.edu/epidata/"
+base_url <- "https://delphi.cmu.edu/epidata/"
