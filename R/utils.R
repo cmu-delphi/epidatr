c585d33213785e5d4b149191484b93a915c5dbdd
Logan C. Brooks <lcbrooks@andrew.cmu.edu>
2023-03-17 19:10:48 -0700

    Revert to old `format_list` logic but fix extra space from `toString`
    * Revert back to old `format_list` logic, which was simpler
    * Fix `format_item` to not insert extra spaces after some commas, which resulted
      in incorrect API requests.
    * Use `vapply` rather than `sapply` in `format_list` to potentially catch weird
      cases and express expectations.
    * Add a few formatting tests.

--- R/utils.R
+++ R/utils.R
@@ -6,17 +6,15 @@ format_item <- function(value) {
     "from" %in% names(value) && "to" %in% names(value) && length(names(value)) == 2) {
     paste0(toString(value$from), "-", toString(value$to))
   } else {
-    toString(value)
+    paste(value, collapse = ",")
   }
 }
 
 # Helper function to build a list of values and/or ranges
 format_list <- function(values) {
-  if (is.vector(values)) {
-    values <- as.list(values)
-  } else if (!is.list(values) ||
+  if (!is.list(values) ||
     ("from" %in% names(values) && "to" %in% names(values))) {
     values <- list(values)
   }
-  paste(sapply(values, format_item), collapse = ",")
+  paste(vapply(values, format_item, character(1L)), collapse = ",")
 }
