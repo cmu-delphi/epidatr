5bccc8e03f0328a19433d96f82c9ba9057b10424
Dmitry Shemetov <dshemetov@ucdavis.edu>
2023-04-05 15:54:34 -0700

    style: auth.R

--- R/auth.R
+++ R/auth.R
@@ -3,28 +3,28 @@
 #' @return The API key
 #' @export
 get_auth_key <- function(verbose = FALSE) {
-    key <- getOption("delphi.epidata.key")
+  key <- getOption("delphi.epidata.key")
+  if (key != "") {
+    if (verbose) print("Key found in delphi.epidata.key option")
+    return(key)
+  }
+
+  key <- Sys.getenv("DELPHI_EPIDATA_KEY")
+  if (key != "") {
+    if (verbose) print("Key found in DELPHI_EPIDATA_KEY environment variable")
+    return(key)
+  }
+
+  if (file.exists("~/.config/delphi_epidata/key")) {
+    key <- readLines("~/.config/delphi_epidata/key")
     if (key != "") {
-        if (verbose) print("Key found in delphi.epidata.key option")
-        return(key)
+      if (verbose) print("Key found in ~/.config/delphi_epidata/key")
+      return(key)
     }
+  }
 
-    key <- Sys.getenv("DELPHI_EPIDATA_KEY")
-    if (key != "") {
-        if (verbose) print("Key found in DELPHI_EPIDATA_KEY environment variable")
-        return(key)
-    }
-
-    if (file.exists("~/.config/delphi_epidata/key")) {
-        key <- readLines("~/.config/delphi_epidata/key")
-        if (key != "") {
-            if (verbose) print("Key found in ~/.config/delphi_epidata/key")
-            return(key)
-        }
-    }
-
-    warning("No key found. Please set one with set_auth_key() to avoid rate limits.")
-    return("")
+  warning("No key found. Please set one with set_auth_key() to avoid rate limits.")
+  return("")
 }
 
 #' @title Set the API key
@@ -32,14 +32,14 @@ get_auth_key <- function(verbose = FALSE) {
 #'   ~/.config/delphi_epidata/key
 #' @export
 set_auth_key <- function() {
-    key <- readline("Enter your key: ")
-    confirmation <- readline("Do you want to store the key in ~/.config/delphi_epidata/key? [y/n] ")
-    if (confirmation != "y") {
-        stop("Key not saved.")
-    } else {
-        dir.create("~/.config/delphi_epidata", showWarnings = FALSE, recursive = TRUE)
-        print("Key saved to ~/.config/delphi_epidata/key")
-        writeLines(key, "~/.config/delphi_epidata/key")
-    }
-    options(delphi.epidata.key = key)
+  key <- readline("Enter your key: ")
+  confirmation <- readline("Do you want to store the key in ~/.config/delphi_epidata/key? [y/n] ")
+  if (confirmation != "y") {
+    stop("Key not saved.")
+  } else {
+    dir.create("~/.config/delphi_epidata", showWarnings = FALSE, recursive = TRUE)
+    print("Key saved to ~/.config/delphi_epidata/key")
+    writeLines(key, "~/.config/delphi_epidata/key")
+  }
+  options(delphi.epidata.key = key)
 }
