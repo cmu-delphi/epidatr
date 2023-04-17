5a96125a5ad120a441afc0633e115db35740fcc7
Dmitry Shemetov <dshemetov@ucdavis.edu>
2023-04-17 15:11:27 -0700

    style: styler

--- tests/testthat/test-auth.R
+++ tests/testthat/test-auth.R
@@ -10,12 +10,12 @@ test_that("get_auth_key", {
 test_that("authenticate", {
   response <- httr::RETRY("GET", "https://httpbin.org/headers", httr::authenticate("epidata", get_auth_key()))
   expect_equal(
-    content(response)$headers$Authorization, 
+    content(response)$headers$Authorization,
     paste0("Basic ", base64enc::base64encode(charToRaw("epidata:")))
   )
   response <- httr::RETRY("GET", "https://httpbin.org/headers", httr::authenticate("epidata", "epidata"))
   expect_equal(
-    content(response)$headers$Authorization, 
+    content(response)$headers$Authorization,
     paste0("Basic ", base64enc::base64encode(charToRaw("epidata:epidata")))
   )
 })
