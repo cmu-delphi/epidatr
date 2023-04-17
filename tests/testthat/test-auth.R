960988d75986933e2a217574e8ad0295397404b5
Dmitry Shemetov <dshemetov@ucdavis.edu>
2023-04-17 14:01:56 -0700

    bug: fix setenv arg in test-auth

--- tests/testthat/test-auth.R
+++ tests/testthat/test-auth.R
@@ -4,5 +4,5 @@ test_that("get_auth_key", {
     options(delphi.epidata.key = NULL)
     Sys.setenv(DELPHI_EPIDATA_KEY = "epidata")
     expect_equal(get_auth_key(), "epidata")
-    Sys.setenv(DELPHI_EPIDATA_KEY = NULL)
+    Sys.setenv(DELPHI_EPIDATA_KEY = "")
 })
