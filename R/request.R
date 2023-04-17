21d66b20886570bf350adcc1f779fd266e425934
Dmitry Shemetov <dshemetov@ucdavis.edu>
2023-04-17 13:29:22 -0700

    refactor: reorder, organize request params

--- R/request.R
+++ R/request.R
@@ -10,20 +10,20 @@ join_url <- function(url, endpoint) {
 do_request <- function(url, params) {
   # don't retry in case of certain status codes
   res <- httr::RETRY("GET",
-    url,
+    url = url,
     query = params,
+    terminate_on = c(400, 401, 403, 405, 414, 500),
     http_headers,
-    httr::authenticate("epidata", get_auth_key()),
-    terminate_on = c(400, 401, 403, 405, 414, 500)
+    httr::authenticate("epidata", get_auth_key())
   )
   if (res$status_code == 414) {
     res <- httr::RETRY("POST",
-      url,
+      url = url,
       body = params,
       encode = "form",
+      terminate_on = c(400, 401, 403, 405, 414, 500),
       http_headers,
-      httr::authenticate("epidata", get_auth_key()),
-      terminate_on = c(400, 401, 403, 405, 414, 500)
+      httr::authenticate("epidata", get_auth_key())
     )
   }
   res
