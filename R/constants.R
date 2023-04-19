f1cbf370bd7a31e3fb8fae42387237a9b18d6630
Dmitry Shemetov <dshemetov@ucdavis.edu>
2023-04-18 17:10:53 -0700

    feat: add gzip encoding to headers

--- R/constants.R
+++ R/constants.R
@@ -1,3 +1,3 @@
 version <- "1.0.0"
-http_headers <- httr::add_headers("User-Agent" = paste0("epidatr/", version))
+http_headers <- httr::add_headers("User-Agent" = paste0("epidatr/", version), "Accept-Encoding" = "gzip")
 global_base_url <- "https://delphi.cmu.edu/epidata/"
