70a9bbbef747dab064e8dfee91288168212724f2
Dmitry Shemetov <dshemetov@ucdavis.edu>
2023-04-24 14:26:27 -0700

    refactor: clarify and update arg check functions, add tests

--- R/check.R
+++ R/check.R
@@ -1,90 +1,186 @@
-check_string_param <- function(name, value, required = TRUE) {
-  # string or string[]
-  if ((required &&
-    !(!is.null(value) &&
-      is.character(value))) ||
-    (!required && !(is.null(value) || is.character(value)))) {
-    rlang::abort(paste0("argument ", name, " is not a string"),
-      name = name, value = value, class = "invalid_argument"
-    )
-  }
-}
-
-check_int_param <- function(name, value, required = TRUE) {
-  # numeric or numeric[]
-  if ((required &&
-    !(!is.null(value) &&
-      is.numeric(value))) ||
-    (!required && !(is.null(value) || is.numeric(value)))) {
-    rlang::abort(paste0("argument ", name, " is not a number"),
-      name = name, value = value, class = "invalid_argument"
-    )
-  }
-}
-
-is_epirange_like <- function(value) {
-  # character or numeric or EpiRange or list(from=..., to=...)
-  is.character(value) ||
-    is.numeric(value) ||
-    inherits(value, "EpiRange") ||
-    (is.list(value) &&
-      "from" %in% names(value) && "to" %in% names(value))
-}
-
-check_single_epirange_param <- function(name, value, required = TRUE) {
-  if ((required &&
-    !(!is.null(value) &&
-      is_epirange_like(value))) ||
-    (!required && !(is.null(value) || is_epirange_like(value)))) {
-    rlang::abort(paste0("argument ", name, " is not a epirange"),
-      name = name, value = value, class = "invalid_argument"
-    )
-  }
-}
-
-check_epirange_param <- function(name, value, required = TRUE) {
+#' Allows string vectors of length 1
+check_single_string_param <- function(name, value, required = TRUE) {
   if (is.null(value)) {
     if (required) {
-      rlang::abort(paste0("argument ", name, " is not a epirange"),
+      rlang::abort(paste0("argument ", name, " is not a single string"),
         name = name, value = value, class = "invalid_argument"
       )
     }
-    return()
-  }
-  if (is_epirange_like(value)) {
-    return()
-  }
-  if (!is.list || !all(sapply(sapply(value, is_epirange_like)))) {
-    rlang::abort(paste0("argument ", name, " is not a epirange"),
-      name = name, value = value, class = "invalid_argument"
-    )
-  }
-}
-
-check_single_string_param <- function(name, value, required = TRUE) {
-  if ((required &&
-    !(!is.null(value) &&
-      is.character(value) &&
-      length(value) == 1)) ||
-    (!required &&
-      !(is.null(value) ||
-        (
-          is.character(value) && length(value) == 1
-        )))) {
+  } else if (is.character(value)) {
+    if (length(value) != 1) {
+      rlang::abort(paste0("argument ", name, " is not a single string"),
+        name = name, value = value, class = "invalid_argument"
+      )
+    }
+  } else {
     rlang::abort(paste0("argument ", name, " is not a single string"),
       name = name, value = value, class = "invalid_argument"
     )
   }
 }
 
+#' Allows string vectors of length > 1 and lists of string vectors of length 1
+check_string_param <- function(name, value, required = TRUE) {
+  if (is.null(value)) {
+    if (required) {
+      rlang::abort(paste0("argument ", name, " is not a string"),
+        name = name, value = value, class = "invalid_argument"
+      )
+    }
+  } else if (is.character(value)) {
+    return()
+  } else if (is.list(value)) {
+    for (i in seq_along(value)) {
+      if (!is.character(value[[i]]) || length(value[[i]]) != 1) {
+        rlang::abort(paste0("argument ", name, "[", i, "] is not a single string"),
+          name = name, value = value, class = "invalid_argument"
+        )
+      }
+    }
+  } else {
+    rlang::abort(paste0("argument ", name, " is not a string"),
+      name = name, value = value, class = "invalid_argument"
+    )
+  }
+}
+
+#' Allows a single numeric value
 check_single_int_param <- function(name, value, required = TRUE) {
-  if ((required &&
-    !(!is.null(value) &&
-      is.numeric(value) &&
-      length(value) == 1)) ||
-    (!required &&
-      !(is.null(value) || (is.numeric(value) && length(value) == 1)))) {
-    rlang::abort(paste0("argument ", name, " is not a single integer"),
+  if (is.null(value)) {
+    if (required) {
+      rlang::abort(paste0("argument ", name, " is not a single number"),
+        name = name, value = value, class = "invalid_argument"
+      )
+    }
+  } else if (is.numeric(value)) {
+    if (length(value) != 1) {
+      rlang::abort(paste0("argument ", name, " is not a single number"),
+        name = name, value = value, class = "invalid_argument"
+      )
+    }
+  } else {
+    rlang::abort(paste0("argument ", name, " is not a single number"),
+      name = name, value = value, class = "invalid_argument"
+    )
+  }
+}
+
+#' Allows a vector of numeric values or a list of numeric values
+check_int_param <- function(name, value, required = TRUE) {
+  if (is.null(value)) {
+    if (required) {
+      rlang::abort(paste0("argument ", name, " is not a number"),
+        name = name, value = value, class = "invalid_argument"
+      )
+    }
+  } else if (is.numeric(value)) {
+    return()
+  } else if (is.list(value)) {
+    for (i in seq_along(value)) {
+      if (!is.numeric(value[[i]]) || length(value[[i]]) != 1) {
+        rlang::abort(paste0("argument ", name, "[", i, "] is not a single number"),
+          name = name, value = value, class = "invalid_argument"
+        )
+      }
+    }
+  } else {
+    rlang::abort(paste0("argument ", name, " is not a number"),
+      name = name, value = value, class = "invalid_argument"
+    )
+  }
+}
+
+#' Return TRUE if value is datelike
+#' TODO: add Date support
+is_date_like <- function(value) {
+  is.character(value) ||
+    is.numeric(value)
+}
+
+#' Check if param is datelike
+check_single_date_param <- function(name, value, required = TRUE) {
+  if (is.null(value)) {
+    if (required) {
+      rlang::abort(paste0("argument ", name, " is not a datelike"),
+        name = name, value = value, class = "invalid_argument"
+      )
+    }
+  } else if (is_date_like(value) && length(value) == 1) {
+    return()
+  } else {
+    rlang::abort(paste0("argument ", name, " is not a datelike"),
+      name = name, value = value, class = "invalid_argument"
+    )
+  }
+}
+
+#' Check if param is datelike or vector of datelike
+check_date_param <- function(name, value, required = TRUE) {
+  if (is.null(value)) {
+    if (required) {
+      rlang::abort(paste0("argument ", name, " is not a datelike"),
+        name = name, value = value, class = "invalid_argument"
+      )
+    }
+  } else if (is_date_like(value)) {
+    return()
+  } else if (is.list(value) && length(value) > 0) {
+    for (i in seq_along(value)) {
+      check_single_date_param(paste0(name, "[", i, "]"), value[[i]], required = TRUE)
+    }
+  } else {
+    rlang::abort(paste0("argument ", name, " is not a datelike"),
+      name = name, value = value, class = "invalid_argument"
+    )
+  }
+}
+
+#' Return TRUE if value is a character or numeric or EpiRange
+#' TODO: add Date support
+is_epirange_like <- function(value) {
+  is.character(value) ||
+    is.numeric(value) ||
+    inherits(value, "EpiRange")
+}
+
+#' Check if param is a character or numeric or EpiRange
+check_single_epirange_param <- function(name, value, required = TRUE) {
+  if (is.null(value)) {
+    if (required) {
+      rlang::abort(paste0("argument ", name, " is not an epirange_like"),
+        name = name, value = value, class = "invalid_argument"
+      )
+    }
+  } else if (is_epirange_like(value)) {
+    return()
+  } else {
+    rlang::abort(paste0("argument ", name, " is not an epirange_like"),
+      name = name, value = value, class = "invalid_argument"
+    )
+  }
+}
+
+#' Check if param is a character or numeric or EpiRange
+check_epirange_param <- function(name, value, required = TRUE) {
+  if (is.null(value)) {
+    if (required) {
+      rlang::abort(paste0("argument ", name, " is not an epirange_like"),
+        name = name, value = value, class = "invalid_argument"
+      )
+    }
+    return()
+  } else if (is_epirange_like(value)) {
+    return()
+  } else if (all(map(value, typeof) == "list") && length(value) > 0) {
+    for (i in seq_along(value)) {
+      check_single_epirange_param(paste0(name, "[", i, "]"), value[[i]], required = TRUE)
+    }
+  } else if (sum(names(value) %in% c("from", "to")) >= 2) {
+    rlang::abort(paste0("argument ", name, " does not accept vectors of epirange_likes, please use a list instead"),
+      name = name, value = value, class = "invalid_argument"
+    )
+  } else {
+    rlang::abort(paste0("argument ", name, " is not an epirange_like"),
       name = name, value = value, class = "invalid_argument"
     )
   }
