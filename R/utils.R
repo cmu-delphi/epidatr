0c465d61f77df9e03c94e706a8c3d47d9e015e21
Logan C. Brooks <lcbrooks@andrew.cmu.edu>
2023-05-03 11:59:52 -0700

    Use `#'` + `@noRd` for internal function doc comments

--- R/utils.R
+++ R/utils.R
@@ -1,15 +1,19 @@
-#' Helper function to cast values and/or ranges to strings
+#' Helper function to cast values, non-list vectors, and/or EpiRanges to strings
+#'
+#' @noRd
 format_item <- function(value) {
   if (inherits(value, "EpiRange")) {
     paste0(toString(value$from), "-", toString(value$to))
   } else if (inherits(value, "Date")) {
-    format(value, "%Y%m%d")
+    paste(format(value, "%Y%m%d"), collapse = ",")
   } else {
     paste(value, collapse = ",")
   }
 }
 
-# Helper function to build a list of values and/or ranges
+#' Helper function to build a list of values and/or ranges
+#'
+#' @noRd
 format_list <- function(values) {
   paste(vapply(values, format_item, character(1L)), collapse = ",")
 }
