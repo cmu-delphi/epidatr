0b3fae2622381aea29705fa72a6fc0f043e71a29
Dmitry Shemetov <dshemetov@ucdavis.edu>
2023-05-30 17:55:15 -0700

    bug(test): correctly mock requests

--- tests/testthat/test-epidatacall.R
+++ tests/testthat/test-epidatacall.R
@@ -8,12 +8,11 @@ test_that("fetch and fetch_tbl", {
     geo_values = "ca,fl"
   )
 
-  # This test compares the output of a tibble using fetch and fetch_tbl.
-  with_mocked_bindings(
-    {
-      tbl_out <- epidata_call %>% fetch_tbl()
-      out <- epidata_call %>% fetch()
-    },
+  local_mocked_bindings(
+    request_impl = function(...) NULL,
+    .package = "epidatr"
+  )
+  local_mocked_bindings(
     # RDS file generated with
     # epidata_call %>%
     # fetch_debug(format_type = "classic") %>%
@@ -22,6 +21,8 @@ test_that("fetch and fetch_tbl", {
     .package = "httr"
   )
 
+  tbl_out <- epidata_call %>% fetch_tbl()
+  out <- epidata_call %>% fetch()
   expect_identical(out, tbl_out)
 })
 
@@ -34,20 +35,27 @@ test_that("fetch_tbl warns on non-success", {
     time_values = epirange("2020-06-01", "2020-08-01"),
     geo_values = "ca,fl"
   )
-  artificial_warning <- "* This is a warning with a leading asterisk and {braces} to make sure we don't have bulleting/glue bugs."
-  debug_response_content_triplet <-
+
+  local_mocked_bindings(
+    request_impl = function(...) NULL,
+    .package = "epidatr"
+  )
+  local_mocked_bindings(
+    content = function(...) NULL,
+    .package = "httr"
+  )
+  artificial_warning <- "* This is a warning with a leading asterisk and {braces} to make sure we don't have bulleting/glue bugs."  
+  debug_triplet <- readRDS(testthat::test_path("data/test-classic.rds")) %>%
+                              jsonlite::fromJSON() %>%
+                              `[[<-`("message", artificial_warning)
+  local_mocked_bindings(
     # see generation code above
-    readRDS(testthat::test_path("data/test-classic.rds")) %>%
-    jsonlite::fromJSON() %>%
-    `[[<-`("message", artificial_warning)
-  with_mocked_bindings(
-    {
-      expect_warning(epidata_call %>% fetch_tbl(),
-        regexp = paste0("epidata warning: ", artificial_warning),
-        fixed = TRUE
-      )
-    },
-    fromJSON = function(...) debug_response_content_triplet,
+    fromJSON = function(...) debug_triplet,
     .package = "jsonlite"
+  )
+
+  expect_warning(epidata_call %>% fetch_tbl(),
+    regexp = paste0("epidata warning: ", artificial_warning),
+      fixed = TRUE
   )
 })
