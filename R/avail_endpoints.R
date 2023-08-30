8563225d280993460459f1989997a91c60b14458
Daniel McDonald <dajmcdon@gmail.com>
2023-08-30 13:57:47 -0700

    simplify avail_endpoints.R
    See comment on #154

--- R/avail_endpoints.R
+++ R/avail_endpoints.R
@@ -8,13 +8,10 @@
 #' endps <- avail_endpoints()
 #' print(endps, n = nrow(endps))
 avail_endpoints <- function() {
-  db <- utils::hsearch_db("epidatr")
-  endpoint_ids <- db$Concepts$ID[db$Concepts$Concept == "endpoint"]
-  all_ids <- db$Base$ID
-  our_endpoints <- all_ids %in% endpoint_ids
+  h <- help.search("endpoint", package = "epidatr", fields = "concept")$matches
   tib <- tibble::tibble( # printing is much nicer than data.frame
-    Endpoint = paste0(db$Base$Name[our_endpoints], "()"),
-    Description = db$Base$Title[our_endpoints]
+    Endpoint = paste0(h$Name, "()"),
+    Description = h$Title
   )
   tib
 }
