8cc7d5b6677a8720311ff76df76c410da255b820
Nat DeFries <42820733+nmdefries@users.noreply.github.com>
2023-12-07 17:11:54 -0500

    add .9000 everywhere

--- R/constants.R
+++ R/constants.R
@@ -1,3 +1,3 @@
-version <- "1.0.0"
+version <- "1.0.0.9000"
 http_headers <- httr::add_headers("User-Agent" = paste0("epidatr/", version), "Accept-Encoding" = "gzip")
 global_base_url <- "https://api.delphi.cmu.edu/epidata/"
