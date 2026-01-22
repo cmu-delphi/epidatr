fc22e6506d27d58322796ab1ea4c5f039226658d
JavierMtzRdz <javierwolfsangel@gmail.com>
2026-01-21 17:10:08 -0800

    reframe: initial version of do_request using httr2

--- R/request.R
+++ R/request.R
@@ -10,57 +10,52 @@ join_url <- function(url, endpoint) {
 #' You can test the authentication headers like so:
 #' @examplesIf curl::has_internet() && Sys.getenv("DELPHI_EPIDATA_KEY") != ""
 #'
-#' response <- httr::RETRY(
-#'   "GET", "https://httpbin.org/headers",
-#'   httr::authenticate("epidata", "fake_key")
-#' )
-#' httr::content(response)$headers$Authorization == paste0(
+#' response <- httr2::request("https://httpbin.org/headers") %>%
+#'   httr2::req_auth_basic("epidata", "fake_key") %>%
+#'   httr2::req_perform() %>%
+#'   httr2::resp_body_json()
+#' response$headers$Authorization == paste0(
 #'   "Basic ",
 #'   base64enc::base64encode(charToRaw("epidata:fake_key"))
 #' )
 #'
-#' @importFrom httr RETRY
+#' @importFrom httr2 request req_user_agent req_headers req_timeout req_retry req_error req_auth_basic req_url_query req_perform req_method req_body_form resp_status
+#' @importFrom magrittr %>%
 #' @keywords internal
 do_request <- function(url, params, timeout_seconds) {
   # don't retry in case of certain status codes
   key <- get_api_key()
+
+  req <- request(url) %>%
+    req_user_agent(paste0("epidatr/", version)) %>%
+    req_headers("Accept-Encoding" = "gzip") %>%
+    req_timeout(timeout_seconds) %>%
+    req_retry(
+      max_tries = 3,
+      is_transient = function(resp) {
+        !(resp_status(resp) %in% c(400, 401, 403, 405, 414, 500))
+      }
+    ) %>%
+    req_error(is_error = function(resp) FALSE)
+
   if (key != "") {
-    res <- httr::RETRY("GET",
-      url = url,
-      query = params,
-      terminate_on = c(400, 401, 403, 405, 414, 500),
-      http_headers,
-      httr::authenticate("epidata", get_api_key()),
-      httr::timeout(timeout_seconds)
-    )
-  } else {
-    res <- httr::RETRY("GET",
-      url = url,
-      query = params,
-      terminate_on = c(400, 401, 403, 405, 414, 500),
-      http_headers,
-      httr::timeout(timeout_seconds)
-    )
+    req <- req %>% req_auth_basic("epidata", key)
   }
-  if (res$status_code == 414) {
+
+  # Try GET
+  req_get <- req %>% req_url_query(!!!params)
+  res <- req_perform(req_get)
+
+  if (resp_status(res) == 414) {
     if (key != "") {
-      res <- httr::RETRY("POST",
-        url = url,
-        body = params,
-        encode = "form",
-        terminate_on = c(400, 401, 403, 405, 414, 500),
-        http_headers,
-        httr::authenticate("epidata", get_api_key())
-      )
-    } else {
-      res <- httr::RETRY("POST",
-        url = url,
-        body = params,
-        encode = "form",
-        terminate_on = c(400, 401, 403, 405, 414, 500),
-        http_headers
-      )
+      # Auth is already in req
     }
+    # Try POST
+    req_post <- req %>%
+      req_method("POST") %>%
+      req_body_form(!!!params)
+
+    res <- req_perform(req_post)
   }
   res
 }
