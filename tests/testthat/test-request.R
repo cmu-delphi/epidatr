df3209437625901c962f320088620faa4859df4c
JavierMtzRdz <javierwolfsangel@gmail.com>
2026-02-12 11:10:10 -0800

    tests: remove the test from httpbin.org that is always skipped

--- tests/testthat/test-request.R
+++
@@ -1,5 +0,0 @@
-test_that("requesting works", {
-  skip("This site is down.")
-  res <- do_request("https://httpbin.org/status/414", list(), timeout_seconds = 10 * 60)
-  expect_equal(res$status_code, 414)
-})
