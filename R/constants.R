93c901ef197f96ea01091dfd2abe8d2e6ea6472c
JavierMtzRdz <javierwolfsangel@gmail.com>
2026-03-26 00:51:44 -0700

    fead: use pre prod cast-API url

--- R/constants.R
+++ R/constants.R
@@ -1,3 +1,3 @@
 http_headers <- list("Accept-Encoding" = "gzip")
 global_base_url <- "https://api.delphi.cmu.edu/epidata/"
-cast_base_url <- "https://development.delphi.cmu.edu/cast-api/epidata/v2/"
+cast_base_url <- "https://delphi.cmu.edu/cast-api/epidata/v2/"
