efb492a437745548fb7568edfa29e103e6f669fe
JavierMtzRdz <javierwolfsangel@gmail.com>
2026-05-04 15:23:30 -0700

    feat(test): add live tests for cast-API

---
+++ tests/testthat/test-epidata-live.R
@@ -0,0 +1,41 @@
+# This script tests all endpoints in the list of sources
+# provided in cast_sources.
+skip_on_cran()
+skip_if_not(curl::has_internet())
+# Generally we skip this. Run it manually.
+skip()
+
+cast_sources <- c("nssp", "nhsn", "pophive", "nwss")
+
+
+for (source in cast_sources) {
+  test_that(sprintf("test `epidata_meta` for source =%s", source), {
+    
+    source_meta <- epidata_meta(source = source)[[source]]
+    expect_type(source_meta, "list")
+    expect_true(length(source_meta$signals) > 0)
+    expect_true(length(source_meta$geo_types) > 0)
+  })
+  for (signal in source_meta$signals) {
+    test_that(sprintf("epidata_* for source =%s and signal = %s", source, signal), {
+      meta_geo_type <- source_meta$geo_types
+      geo_type <- if ("nation" %in% meta_geo_type) "nation" else meta_geo_type[[1]]
+      snapshot <- epidata_snapshot(
+        source = source,
+        signals = signal,
+        geo_type = geo_type
+      )
+      expect_s3_class(snapshot, "tbl_df")
+      # expect_gt(nrow(snapshot), 0)
+      expect_s3_class(snapshot$time_value, "Date")
+      expect_s3_class(snapshot$version, "Date")
+      archive <- epidata_archive(
+        source = source,
+        signals = signal,
+        geo_type = geo_type
+      )
+      expect_s3_class(archive, "tbl_df")
+      expect_s3_class(archive$version, "Date")
+    })
+  }
+}
