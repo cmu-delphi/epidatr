5e8a49d3bad5a2ef7e3450cb8435b84c3a41b956
JavierMtzRdz <javierwolfsangel@gmail.com>
2026-08-24 19:10:08 -0600

    feat: add V4 deprecation warning to pub_fluview_clinical and update documentation

--- R/endpoints.R
+++ R/endpoints.R
@@ -2366,6 +2366,14 @@ pub_flusurv <- function(
 
 #' CDC FluView flu tests from clinical labs
 #' @description
+#' This is a V4 endpoint. Starting in October 2026, it is tentatively
+#' deprecated in favor of the V5 API. The new API can be accessed via the
+#' [epidata_snapshot()], [epidata_archive()], and [epidata_meta()] functions.
+#' For more details on the changes, refer to `vignette("migration-guide")`,
+#' and visit the [V5 signals
+#' documentation](https://cmu-delphi.github.io/delphi-epidata/api/v5_signals.html)
+#' to see which sources are currently available.
+#'
 #' API docs: <https://cmu-delphi.github.io/delphi-epidata/api/fluview_clinical.html>
 #'
 #' @examplesIf curl::has_internet() && Sys.getenv("DELPHI_EPIDATA_KEY") != ""
@@ -2396,6 +2404,7 @@ pub_fluview_clinical <- function(
   fetch_args = fetch_args_list()
 ) {
   rlang::check_dots_empty()
+  warn_v4_sunset("pub_fluview_clinical")
 
   epiweeks <- get_wildcard_equivalent_dates(epiweeks, "week")
 
